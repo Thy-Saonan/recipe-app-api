811a951c5385e5ba5fc0172a050f8e92decc64da
Saonan <thysaonan007@gmail.com>
2021-04-21 15:04:59 +0700

    Added list and create ingredients feature.

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -77,7 +77,7 @@ class PrivateTagsApiTests(TestCase):
         self.assertTrue(exist)
 
     def test_create_tag_invalid(self):
-        """Test creating a new tag witH invalid payload"""
+        """Test creating a new tag with invalid payload"""
         payload = {'name': ''}
         res = self.client.post(TAGS_URL, payload)
 
